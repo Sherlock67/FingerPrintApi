32cb4cfcd993dbd65a7e67f072caa23f3db735c5
Sherlock67 <alammushfiqul366@gmail.com>
2022-05-08 01:34:25 +0600

    added repository

---
+++ FingerprintDataAcessLayer/Interface/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerprintDataAcessLayer.Interface
+{
+    public interface IRepository<T> 
+    {
+
+        public Task<T> Create(T entity);
+       
+
+    }
+}
---
+++ FingerprintDataAcessLayer/Repository/FingerprintRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using FingerprintAPI.Data;
+using FingerprintAPI.Model;
+using FingerprintDataAcessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerprintDataAcessLayer.Repository
+{
+    public class FingerprintRepository : IFingerprintSave
+    {
+        private readonly ApplicationDbContext _db;
+        public FingerprintRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<FingerPrint> Create(FingerPrint entity)
+        {
+            var obj = await _db.fingerprints.AddAsync(entity);
+            _db.SaveChanges();
+            return obj.Entity;
+        }
+    }
+}
