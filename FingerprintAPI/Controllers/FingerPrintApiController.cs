058375d803b9366c5522a6254c7f967c6ab69668
Sherlock67 <alammushfiqul366@gmail.com>
2022-05-07 15:36:10 +0600

    data access layer added

---
+++ FingerprintAPI/Controllers/FingerPrintApiController.cs
@@ -0,0 +1,35 @@
+﻿using FingerprintAPI.Data;
+using FingerprintAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FingerprintAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class FingerPrintApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public FingerPrintApiController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        [HttpPost("FingerPrint")]
+        public async Task<Object> CreatePrescription([FromBody] FingerPrint fingerPrint)
+        {
+            try
+            {
+                await _db.fingerprints.Add(fingerPrint);
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
